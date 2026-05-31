3cecb8df14539011ad81c9294396348f156c55da
motsoane012 <ranchobemotsoane48@gmail.com>
2026-05-31 16:34:03 +0200

    Delete SupabaseClient.js

--- SupabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
-
-const SUPABASE_URL = 'https://olwhjgfuctdtltvbfpie.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_yKOn_41eJDOQ2IQYkeSKFg_Z206p5jW'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
